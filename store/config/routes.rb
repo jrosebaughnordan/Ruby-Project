49dbbe0a6a6a93d46ae90e79a784aa86ec2b5905
jrosebaughnordan <65576732+jrosebaughnordan@users.noreply.github.com>
2025-10-10 14:27:08 -0400

    email subscriptions and unsubscribe working

--- store/config/routes.rb
+++ store/config/routes.rb
@@ -7,8 +7,10 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  resources :products
-
+  resources :products do
+    resources :subscribers, only: [:create]
+  end
+  resource :unsubscribe, only: [ :show ]
   # get "/products", to: "products#index"
   # get "/products/new", to: "products#new"
   # post "/products", to: "products#create"
---
+++ store/db/migrate/20251010145416_add_inventoiry_count_to_products.rb
@@ -0,0 +1,5 @@
+class AddInventoiryCountToProducts < ActiveRecord::Migration[8.0]
+  def change
+    add_column :products, :inventory_count, :integer
+  end
+end
